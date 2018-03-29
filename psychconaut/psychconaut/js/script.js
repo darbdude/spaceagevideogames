5dec16c9c55b73e3f6cd24ded7942369788a32d3
darbdude <darbdude@users.noreply.github.com>
2018-03-28 17:35:47 -0700

    genesis

---
+++ psychconaut/psychconaut/js/script.js
@@ -0,0 +1,26 @@
+﻿room.onLoad = function () {
+    room.playSound("backgroundsound01");
+}
+
+room.update = function (dt) {
+
+}
+
+function LightOff() {
+    room.objects["OmniLight"].light_intensity = 0;
+}
+
+function LightOn() {
+    room.objects["OmniLight"].light_intensity = 200;
+}
+
+function SetLightProperties() {
+    room.objects["OmniLight"].col = "#2668b2";
+    room.objects["OmniLight"].light_intensity = 16;
+}
+
+function StartMusic() {
+
+    room.playSound("backgroundsound01");
+
+}
